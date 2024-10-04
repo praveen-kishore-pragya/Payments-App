a11886de6045a89ba7e2683a8b66f06bd049c653
praveen-kishore-pragya <praveen.pragya1@gmail.com>
2024-10-04 19:51:03 +0530

    Added DB config

--- backendPaymentsApp/db.js
+++ backendPaymentsApp/db.js
@@ -1,1 +1,36 @@
 const mongoose = require('mongoose')
+
+mongoose.connect(`${MY_MONGO_DB_URL}`);
+
+const userSchema = new mongoose.Schema(
+    {
+        username : {
+            type: String,
+            required: true,
+        },
+        
+        password : {
+            type : String,
+            required : true,
+        },
+        
+        firstName : {
+            type : String,
+            required : true,
+        },
+
+        lastName : {
+            type : String,
+            required : false,
+        },
+    },
+    {
+        timestamps : true
+    }
+);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = {
+    User
+};
